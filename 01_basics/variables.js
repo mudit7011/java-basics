a0e1cb4fad332255803808f26f130645c6867593
Mudit Kumar <115485952+mudit7011@users.noreply.github.com>
2024-09-09 20:05:43 +0000

    commit

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 1234567892344
+let accountEmail = "Muditchaudahry90@gmail.com"
+var accountPass = "12344"
+accontCity = "Noida"
+
+// accountId = 2  this is not allowed in const keyword
+ 
+accountEmail="hchc@gmail.com"
+accountPass = "212121"
+accontCity="bulandshahr"
+
+console.table([accountId,accountEmail,accountPass,accontCity])
